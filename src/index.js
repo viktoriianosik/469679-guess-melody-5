671f9d5404992bca768646d3f0240c7a607f8ac1
Viktoriia <viktoria.nosik@gmail.com>
2020-09-29 12:51:32 +0300

    add app and welcome-screen components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/app/app';
 
+const Settings = {
+  ERRORS_COUNT: 3,
+};
+
+ReactDom.render(
+    <App errorsCount = {Settings.ERRORS_COUNT} />,
+    document.querySelector(`#root`)
+);
