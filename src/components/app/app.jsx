1dc84448579aec1fe3383603d9d151d4c9fc7da1
Viktoriia <viktoria.nosik@gmail.com>
2020-10-02 11:13:05 +0300

    add react-router-dom

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import WelcomeScreen from '../welcome-screen/welcome-screen';
+import React from "react";
+import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import WelcomeScreen from "../welcome-screen/welcome-screen";
+import Login from "../login/login";
+import Lose from "../lose/lose";
+import QuestionArtist from "../question-artist/question-artist";
+import QuestionGenre from "../question-genre/question-genre";
+import ResultSuccess from "../result-success/result-success";
 
 const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen errorsCount = {errorsCount}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen errorsCount={errorsCount}/>
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/lose">
+          <Lose />
+        </Route>
+        <Route exact path="/result">
+          <ResultSuccess />
+        </Route>
+        <Route exact path="/dev-artist">
+          <QuestionArtist />
+        </Route>
+        <Route exact path="/dev-genre">
+          <QuestionGenre />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
